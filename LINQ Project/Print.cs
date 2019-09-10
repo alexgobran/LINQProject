14282b264b9f1ff7fba5b03d8cbb669b57b40aca
alexgobran <54282581+alexgobran@users.noreply.github.com>
2019-09-10 16:09:08 -0400

    add print class, used query syntax and method syntax, joined tables

---
+++ LINQ Project/Print.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CSharp2SqlLibrary;
+
+namespace ExtensionMethods {
+    static class MyExtensionMethods {
+
+
+
+        // has to be public , static , and void
+        public static void PrintBrief(this Product product) {
+            Console.WriteLine($"Product name is {product.Name}");
+        }
+        //    public static void Print(this List<Product> products) {
+
+        //    foreach (var product in products)
+        //        Console.WriteLine(product);
+        //}
+
+        //public static void Print(this List<Vendor> Vendors) {
+
+        //    foreach (var vendor in Vendors)
+        //    {
+        //        Console.WriteLine(vendor);
+        //    }
+
+
+
+        //}
+
+
+
+    }
+
+}
+
+
+
--- LINQ Project/Program.cs
+++ LINQ Project/Program.cs
@@ -1,47 +1,148 @@
 ﻿using System;
 using System.Linq;
+using CSharp2SqlLibrary;
+using ExtensionMethods;
 
 namespace LINQ_Project {
 
-    class User {
-
-        public string Name { get; set;}
-        public bool isAdmin { get; set;}
-
-    }
+  
     class Program {
+        void Run() {
+            //Create connection use @ to allow \ to be used , open connection
+            var Connection = new Connection(@"localhost\sqlexpress","PRS");
+            Connection.Open();
+            User.Connection = Connection;
+            Vendor.Connection = Connection;
+            Product.Connection = Connection;
+
+
+
+           
+
+
+
+            //var states = new State[]
+            //{
+            //    new State
+            //}
+           
+
+            var users = from u in User.GetAll()
+                        where u.Username.Contains("2")
+                        select u;
+
+
+            var admins = from u in User.GetAll()
+                         //use .Equals when referring to string instead of ==
+                         where u.Username.Equals("ABC")
+                         select u;
+
+            var reviewers = from r in User.GetAll()
+                            where r.IsReviewer
+                            select r;
+
+            var vendors = from v in Vendor.GetAll()
+                          orderby v.Name descending
+                            select v;
+            
+
+            #region Print Method/Product Join with Vendor 
+            //print class contains print method which is an extension method
+            Product.Connection = Connection;
+        var products = Product.GetAll();
+            foreach (var product in products)
+            {
+                product.PrintBrief();
+
+            }
+
+            //Vendor.Connection = Connection;
+            //var Vendors = Vendor.GetAll();
+            //Vendors.Print();
+            //#endregion
+
+
+            //Product.Connection = Connection;
+            //var products = from p in Product.GetAll()
+            //               join v in Vendor.GetAll()
+            //               on p.VendorId equals v.Id
+            //               select new
+            //               {
+            //                   Product = p.Name,
+            //                   Price = p.Price,
+            //                   Vendor = v.Name,
+
+            //               };
+            //foreach (var p in products)
+            //{
+            //    Console.WriteLine($"{p.Product} priced at {p.Price} is from {p.Vendor}");
+            //}
+        }
+
+
+        #endregion
+
+        #region Total all products prices
+        //Products.Connection = Connection;
+        //  var totalAllProducts = (jfrom p in Product.GetAll())
+        #endregion
+
+        //var admins = from u in User.GetAll()4
+        //                 where u.IsAdmin
+        //                select u;
+
+        //    foreach (var user in admins)
+        //        {
+        //            Console.WriteLine($"{user.Firstname}{user.Lastname} is an Admin");
+        //        }
+        //        Connection.Close();
+
+        //    foreach( var v in vendors)
+        //    {
+        //        Console.WriteLine(v);
+        //    }
+        //}
+
+
+
+
+
+
         static void Main(string[] args) {
 
-            var users = new User[]
-            {//Collection of users
-                new User(){Name="Adam", isAdmin = false},
-                new User(){Name ="Barb", isAdmin = true},
-                new User(){Name ="Chris", isAdmin = false},
-                new User(){Name ="Donna", isAdmin = true},
-                new User(){Name ="Ed", isAdmin = false},
-                new User(){Name ="Faith", isAdmin = true},
-            };
-            //Method syntax
-            var admins = users.Where(u => u.isAdmin).OrderByDescending(u=> u.Name);
+            var pgm = new Program();
+            pgm.Run();
 
-            //Query Syntax
-            //var admins = from USR in users
-            //             where USR.isAdmin == true
-            //                orderby USR.Name descending;
-            //             select USR;
+            //var users = new User[]
+            //{//Collection of users
+            //    new User(){Name="Adam", isAdmin = false},
+            //    new User(){Name ="Barb", isAdmin = true},
+            //    new User(){Name ="Chris", isAdmin = false},
+            //    new User(){Name ="Donna", isAdmin = true},
+            //    new User(){Name ="Ed", isAdmin = false},
+            //    new User(){Name ="Faith", isAdmin = true},
+            //};
+            ////Method syntax
+            //var admins = users.Where(u => u.isAdmin).OrderByDescending(u=> u.Name);
 
-            //user in the parameters is a Fred variable
-            foreach(var user in admins)
-            {
-                Console.WriteLine($"Name is {user.Name} is  an admin");
-            }
+            ////Query Syntax
+            ////var admins = from USR in users
+            ////             where USR.isAdmin == true
+            ////                orderby USR.Name descending;
+            ////             select USR;
+
+            ////user in the parameters is a Fred variable
+            //foreach(var user in admins)
+            //{
+            //    Console.WriteLine($"Name is {user.Name} is  an admin");
+            //}
 
            // //array - collection of integars)
            // int[] ints = { 2, 4, 6, 8, 7, 5, 3, 1 };
 
            // //below Defined query against our variable but does not execute it! you have to reference it somewhere else in your code
            // //operation below is getting numbers from ints collection with only odd numbers under 7 in descending order
-           // // get average (.Average(i=>i) using Lambda syntax
+           // get average (.Average(i=>i) using Lambda syntax
 
            // //Query syntax
 
